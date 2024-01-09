59210e09c2d35baeed4364cce298f89a76f39f25
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2024-01-09 23:51:58 +0300

    entities are added

---
+++ SignalREntityLayer/Entities/About.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class About
+    {
+        public int ID { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; } 
+
+
+
+    }
+}
