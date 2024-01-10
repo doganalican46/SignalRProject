6dbc01c298b60763c6212ed0d093dc050fba74bb
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2024-01-10 12:47:01 +0300

    data acccess layer created

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace SignalR.DataAccessLayer.Concrete
 {
-    internal class SignalRContext
+    internal class SignalRContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=initial Catalog=SignalRDB; integrated Security=true");
+
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+
     }
 }
