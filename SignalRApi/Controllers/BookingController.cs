7132c24f97ae7f4a196f5e97b4eda653f2887631
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2024-01-16 00:02:05 +0300

    category controller

--- SignalRApi/Controllers/BookingController.cs
+++ SignalRApi/Controllers/BookingController.cs
@@ -1,72 +1,75 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using SignalR.BusinessLayer.Abstract;
-using SignalR.DtoLayer.BookingDto;
-using SignalR.EntityLayer.Entities;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using SignalR.BusinessLayer.Abstract;
+//using SignalR.DtoLayer.BookingDto;
+//using SignalR.EntityLayer.Entities;
 
-namespace SignalRApi.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BookingController : ControllerBase
-    {
-        private readonly IBookingService _bookingService;
+//namespace SignalRApi.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class BookingController : ControllerBase
+//    {
+//        private readonly IBookingService _bookingService;
 
-        public BookingController(IBookingService bookingService)
-        {
-            _bookingService = bookingService;
-        }
-        [HttpGet]
-        public IActionResult BookingList()
-        {
-            var values=_bookingService.BGetListAll();
-            return Ok(values);  
-        }
+//        public BookingController(IBookingService bookingService)
+//        {
+//            _bookingService = bookingService;
+//        }
+//        [HttpGet]
+//        public IActionResult BookingList()
+//        {
+//            var values = _bookingService.BGetListAll();
+//            return Ok(values);
+//        }
 
-        [HttpPost]
-        public IActionResult CreateBooking(CreateBookingDto createBookingDto) {
-            Booking booking = new Booking()
-            {
-                Mail=createBookingDto.Mail,
-                Date=createBookingDto.Date, 
-                Name=createBookingDto.Name,
-                PersonCount=createBookingDto.PersonCount,
-                Phone=createBookingDto.Phone,
+//        [HttpPost]
+//        public IActionResult CreateBooking(CreateBookingDto createBookingDto)
+//        {
+//            Booking booking = new Booking()
+//            {
+//                Mail = createBookingDto.Mail,
+//                Date = createBookingDto.Date,
+//                Name = createBookingDto.Name,
+//                PersonCount = createBookingDto.PersonCount,
+//                Phone = createBookingDto.Phone,
 
-            };
-            _bookingService.BAdd(booking);
-            return Ok("Rezervasyon yapıldı...");
-        }
+//            };
+//            _bookingService.BAdd(booking);
+//            return Ok("Rezervasyon yapıldı...");
+//        }
 
-        [HttpGet]
-        public IActionResult DeleteBooking(int id) { 
-            var value=_bookingService.BGetByID(id);
-            _bookingService.BDelete(value);
+//        [HttpGet]
+//        public IActionResult DeleteBooking(int id)
+//        {
+//            var value = _bookingService.BGetByID(id);
+//            _bookingService.BDelete(value);
 
-            return Ok("Rezervasyon silindi...");   
-        }
+//            return Ok("Rezervasyon silindi...");
+//        }
 
-        [HttpPut]
-        public IActionResult UpdateBooking(UpdateBookingDto updateBookingDto)
-        {
-            Booking booking = new Booking()
-            {
-                Mail=updateBookingDto.Mail,
-                ID=updateBookingDto.ID,
-                Name=updateBookingDto.Name,
-                PersonCount=updateBookingDto.PersonCount,
-                Phone=updateBookingDto.Phone,
-                Date=updateBookingDto.Date,
-            };   
-            _bookingService.BUpdate(booking);
-            return Ok("Rezervasyon başarıyla güncellendi...");
+//        [HttpPut]
+//        public IActionResult UpdateBooking(UpdateBookingDto updateBookingDto)
+//        {
+//            Booking booking = new Booking()
+//            {
+//                Mail = updateBookingDto.Mail,
+//                ID = updateBookingDto.ID,
+//                Name = updateBookingDto.Name,
+//                PersonCount = updateBookingDto.PersonCount,
+//                Phone = updateBookingDto.Phone,
+//                Date = updateBookingDto.Date,
+//            };
+//            _bookingService.BUpdate(booking);
+//            return Ok("Rezervasyon başarıyla güncellendi...");
 
-        }
+//        }
 
-        [HttpGet("GetBooking")]
-        public IActionResult GetBooking(int id) {
-            var value = _bookingService.BGetByID(id);
-            return Ok(value);   
-                }
-    }
-}
+//        [HttpGet("GetBooking")]
+//        public IActionResult GetBooking(int id)
+//        {
+//            var value = _bookingService.BGetByID(id);
+//            return Ok(value);
+//        }
+//    }
+//}
---
+++ SignalRApi/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        private readonly IMapper _mapper; 
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList() {
+            var value = _mapper.Map<List<ResultCategoryDto>>(_categoryService.BGetListAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            _categoryService.BAdd(new Category()
+            {
+                Name = createCategoryDto.Name,
+                Status = true
+            });
+            return Ok("Kategori Eklendi...");
+        }
+        
+    }
+}
