0b10062d15daf2ce2792944f83656334338179f9
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2024-01-15 22:21:09 +0300

    builder.Services

--- SignalR.BusinessLayer/Concrete/BookingManager.cs
+++ SignalR.BusinessLayer/Concrete/BookingManager.cs
@@ -1,45 +1,45 @@
-﻿using SignalR.BusinessLayer.Abstract;
-using SignalR.DataAccessLayer.Abstract;
-using SignalR.EntityLayer.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using SignalR.BusinessLayer.Abstract;
+//using SignalR.DataAccessLayer.Abstract;
+//using SignalR.EntityLayer.Entities;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SignalR.BusinessLayer.Concrete
-{
-    public class BookingManager : IBookingService
-    {
-        private readonly IBookingDal _bookingDal;
-        public BookingManager(IBookingDal bookingDal)
-        {
-            _bookingDal = bookingDal;
-        }
+//namespace SignalR.BusinessLayer.Concrete
+//{
+//    public class BookingManager : IBookingService
+//    {
+//        private readonly IBookingDal _bookingDal;
+//        public BookingManager(IBookingDal bookingDal)
+//        {
+//            _bookingDal = bookingDal;
+//        }
 
-        public void BAdd(Booking entity)
-        {
-            _bookingDal.Add(entity);
-        }
+//        public void BAdd(Booking entity)
+//        {
+//            _bookingDal.Add(entity);
+//        }
 
-        public void BDelete(Booking entity)
-        {
-            _bookingDal.Delete();
-        }
+//        public void BDelete(Booking entity)
+//        {
+//            _bookingDal.Delete();
+//        }
 
-        public Booking BGetByID(int id)
-        {
-            return _bookingDal.GetByID(id); 
-        }
+//        public Booking BGetByID(int id)
+//        {
+//            return _bookingDal.GetByID(id); 
+//        }
 
-        public List<Booking> BGetListAll()
-        {
-            return _bookingDal.GetListAll();
-        }
+//        public List<Booking> BGetListAll()
+//        {
+//            return _bookingDal.GetListAll();
+//        }
 
-        public void BUpdate(Booking entity)
-        {
-            _bookingDal.Update(entity);
-        }
-    }
-}
+//        public void BUpdate(Booking entity)
+//        {
+//            _bookingDal.Update(entity);
+//        }
+//    }
+//}
